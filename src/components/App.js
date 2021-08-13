1aca2ba671665788b4f01acc08011d6057026071
Jun Hee Lee <junheelee@Junui-iMac.local>
2021-08-13 15:30:20 +0900

    #2.6 Log Out

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,11 @@ import { authService } from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  console.log("init: ", init);
+  console.log("isLoggedIn: ", isLoggedIn);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      console.log(user);
+      console.log("user: ", user);
       if (user) {
         setIsLoggedIn(true);
       } else {
