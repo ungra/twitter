a9e5594668e47d27640a0e2122f0ea37838bd297
Jun Hee Lee <junheelee@Junui-iMac.local>
2021-08-30 17:54:20 +0900

    #5.1 Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,39 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDiaplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDiaplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display Name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
