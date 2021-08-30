9ec7f4b72c792aaa003c68ac57d9bf0f3301e7c1
Jun Hee Lee <junheelee@Junui-iMac.local>
2021-08-30 13:51:35 +0900

    #4.3 File URL and Nweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -45,6 +45,9 @@ const Tweet = ({ tweetObj, isOwner }) => {
       ) : (
         <>
           <h4>{tweetObj.text}</h4>
+          {tweetObj.attachmentUrl && (
+            <img src={tweetObj.attachmentUrl} width="50px" height="50px" />
+          )}
           {isOwner && (
             <>
               <button onClick={onDeleteClick}>Delete Tweet</button>
